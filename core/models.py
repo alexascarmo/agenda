eec97cbd0f5324ebdd9a080563f2e2e340572834
Alex Carmo(ACBZ) <Alex_Carmo@corpnet.asus>
2020-10-29 00:18:34 -0300

    Alteracao do campo Local dos Eventos para aceitar branco e nulo

--- core/models.py
+++ core/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Evento(models.Model):
     titulo = models.CharField(max_length=100)
     descricao = models.TextField(blank=True, null=True)
-    local = models.CharField(max_length=130)
+    local = models.CharField(max_length=130, blank=True, null=True)
     data_evento = models.DateTimeField(verbose_name='Data do Evento')
     data_criacao = models.DateTimeField(auto_now=True)
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
